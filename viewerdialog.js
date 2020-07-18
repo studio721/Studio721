aaaebf29dc5ca681cd48b7e8a0b18c3a76cb30c3
Xoshil Chen-Marquez <xoshilcm@gmail.com>
2020-07-18 10:44:47 -0700

    displayed yt video w/o playback controls and w/ time code

--- viewerdialog.js
+++ viewerdialog.js
@@ -1,6 +1,6 @@
 document.getElementsByClassName("tablink")[0].click();
 
-function buttonClicked() {
+function buttonClick() {
 
     var url = document.getElementById("vid-url").value;
     if (url != null && url != "") {
@@ -16,12 +16,14 @@ function buttonClicked() {
 
 function validateYouTubeUrl(url) {
     //checks if valid YouTube url
+    var timeCode = document.getElementById("vid-time").value;  // your input string
+    var seconds = timeCode.split(':').reverse().reduce((prev, curr, i) => prev + curr*Math.pow(60, i), 0)
     var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
     var match = url.match(regExp);
     if (match && match[2].length == 11) {
         //adds url as iframe src
         document.getElementById("video-object").setAttribute(
-            'src', 'https://www.youtube.com/embed/' + match[2] + '?modestbranding=1&controls=0"autoplay=1&enablejsapi=1');
-            document.getElementById("dialog-box").style.display = "none";
+            'src', 'https://www.youtube.com/embed/' + match[2] + '?start=' + seconds + '&modestbranding=1&controls=0&autoplay=1&enablejsapi=1');
+            document.getElementById("viewer-dialog-box").style.display = "none";
     }
 }
